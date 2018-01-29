8f7b53712a8b52d9a00f0137001d3ee4bac15f7b
Zlenko Valeriy <valeriy.zlenko@onix-systems.com>
2018-01-29 16:44:51 +0200

    Part-1

---
+++ src/main/java/Animal.java
@@ -0,0 +1,10 @@
+public abstract class Animal {
+
+    private Integer age = 20;
+    private Integer weight = 30;
+
+    abstract void run();
+    abstract void eat();
+    abstract void sleep();
+
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+public class Cat extends Animal implements Meowable {
+    private String name = "Vasya";
+
+    void run() {
+
+    }
+
+    void eat() {
+
+    }
+
+    void sleep() {
+
+    }
+
+    public void meow() {
+
+    }
+}
